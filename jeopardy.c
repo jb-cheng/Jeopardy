3c705fec08a2748bb83bf5e957935229c835e61c
Anuj711 <anuj.shukla@live.com>
2025-02-07 15:45:46 -0500

    Added name and student number

--- jeopardy.c
+++ jeopardy.c
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
- *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Group Members:
+ * Anuj Shukla (100895219)
  * All rights reserved.
  *
  */
