23809428ce2762bf545e20c359061592dd70d50e
Anuj711 <anuj.shukla@live.com>
2025-02-10 20:11:06 -0500

    Updated jeopardy.c

--- questions.h
+++ questions.h
@@ -39,7 +39,7 @@ extern void initialize_questions(void);
 extern void display_categories(void);
 
 // Displays the question for the category and dollar value
-extern void display_question(char *category, int value);
+extern bool display_question(char *category, int value);
 
 // Returns true if the answer is correct for the question for that category and dollar value
 extern bool valid_answer(char *category, int value, char *answer, char *type);
